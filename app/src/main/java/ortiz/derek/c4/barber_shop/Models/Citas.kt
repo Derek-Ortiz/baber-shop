e4621735abaa6120bc3c765c5e4534c6bbe1fc9b
Derek-Ortiz <243742@ids.upchiapas.edu.mx>
2025-11-16 20:58:47 -0600

    Creacion de models citas y referencia a la llave foranea

--- app/src/main/java/ortiz/derek/c4/barber_shop/Models/Citas.kt
+++ app/src/main/java/ortiz/derek/c4/barber_shop/Models/Citas.kt
@@ -2,10 +2,20 @@ package ortiz.derek.c4.barber_shop.Models
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.Companion.CASCADE
 import androidx.room.PrimaryKey
 import java.sql.Date
 
-@Entity (tableName = "citas")
+@Entity (tableName = "citas",
+    foreignKeys = [
+        ForeignKey(
+            entity = Cliente::class,
+            parentColumns = ["id"],
+            childColumns = ["id_cliente"],
+            onDelete = CASCADE
+        )
+    ])
 data class Citas(
     @PrimaryKey(autoGenerate = true)
     val idCitas: Int,
@@ -17,4 +27,6 @@ data class Citas(
     val precio: Float,
     @ColumnInfo("asunto")
     val asunto: String,
+    @ColumnInfo("id_cliente")
+    val idCliente: Int
 )
